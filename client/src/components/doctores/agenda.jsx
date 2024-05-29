a892de78816b445d49db61c22c6f74252ca7252c
proyectos-developer <proyectos@developer-ideas.com>
2024-05-29 16:15:35 -0500

    V1.0.0

--- client/src/components/doctores/agenda.jsx
+++ client/src/components/doctores/agenda.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import {Inject, Day, Week, WorkWeek, Month, Agenda, ScheduleComponent} from '@syncfusion/ej2-react-schedule'
+
 export default function AgendaDoctores({proporcional}) {
-        
+
     return (
-        <div>
-        
+        <div style={{width: '100%', height: 'auto'}}>
+            <ScheduleComponent 
+                currentView='Month'
+                selectedDate={new Date()}>
+                <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+            </ScheduleComponent>
         </div>
     )
 }
